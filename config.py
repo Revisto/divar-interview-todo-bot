c0e0391fb50aac25991af71405555eb3f0129841
Revisto <theRevisto@gmail.com>
2025-05-17 21:56:20 +0330

    Refactor Config class to include optional DIVAR_OAUTH_SECRET and update DIVAR_REDIRECT_URI

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 
 class Config:
+    BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+
     DIVAR_API_KEY = os.getenv("DIVAR_API_KEY")
     DIVAR_APP_SLUG = os.getenv("DIVAR_APP_SLUG")
+    DIVAR_OAUTH_SECRET = os.getenv("DIVAR_OAUTH_SECRET", "")
+    DIVAR_REDIRECT_URI = f"{BASE_URL}/divar/oauth/callback"
